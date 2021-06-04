c5f25037911e3cd88e2be7c1c8549a371c0ff891
amanda-delongchamp <amanda.delongchamp@gmail.com>
2021-06-04 12:15:43 -0700

    Add project files.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> games = new List<string> {"Mario Brothers","Pacman","Zelda",
+            "Dungeons and Dragons","Tomb Raider","Space Invaders","Centipede"};
+
+            IEnumerable<string> gameNameLength = games.OrderBy(game => game.Length);
+
+            foreach (var g in gameNameLength)
+            {
+                Console.WriteLine(g);
+            }
+            
+            //var gameNameLength = games.OrderBy(game => game.Length);
+
+            //Console.WriteLine(gameNameLength);
+
+        }
+    }
+}
